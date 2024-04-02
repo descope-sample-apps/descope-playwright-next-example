f55a35e9d0d4f52359379dc8b5140c591cca5b75
Kevin Gao <kevinjamesgao@gmail.com>
2024-04-01 17:58:21 -0700

    removed api endpoint test

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -15,22 +15,22 @@ test("Access Protected Page", async ({ page }) => {
   await expect(page.getByText(/Hello, U2/i)).toBeVisible();
 });
 
-test("Test API endpoint", async ({ page }) => {
-  const localStorage = await page.evaluate(() => localStorage.getItem("DS"));
-  const token = JSON.parse(localStorage).split(".")[1];
+// test("Test API endpoint", async ({ page }) => {
+//   const localStorage = await page.evaluate(() => localStorage.getItem("DS"));
+//   const token = JSON.parse(localStorage).split(".")[1];
 
-  const response = await page.request(
-    `${process.env.PLAYWRIGHT_TEST_BASE_URL}/test-api`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  );
+//   const response = await page.request(
+//     `${process.env.PLAYWRIGHT_TEST_BASE_URL}/test-api`,
+//     {
+//       method: "GET",
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     }
+//   );
 
-  // Assert on the response status and body
-  expect(response.status()).toBe(200);
-  const data = await response.json();
-  expect(data).toHaveProperty("sub");
-});
+//   // Assert on the response status and body
+//   expect(response.status()).toBe(200);
+//   const data = await response.json();
+//   expect(data).toHaveProperty("sub");
+// });
